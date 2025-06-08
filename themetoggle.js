ad35e4d11c61db9484978787e4f3aaeb3f13ac24
Sainath307 <ssainath657@gmail.com>
2025-06-08 21:57:53 +0530

    Create themetoggle.js

---
+++ themetoggle.js
@@ -0,0 +1,29 @@
+
+import React, { useEffect, useState } from 'react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+const ThemeToggle = () => {
+  const [darkMode, setDarkMode] = useState(() => localStorage.getItem('theme') === 'dark');
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [darkMode]);
+
+  return (
+    <button
+      onClick={() => setDarkMode(!darkMode)}
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-700"
+      title="Toggle Theme"
+    >
+      {darkMode ? <FaSun className="text-yellow-400" /> : <FaMoon className="text-gray-800" />}
+    </button>
+  );
+};
+
+export default ThemeToggle;
